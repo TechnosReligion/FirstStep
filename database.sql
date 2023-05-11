6f724cf3e43f4acf27e00457b4192e9c0aace36c
TechnosReligion <132928129+TechnosReligion@users.noreply.github.com>
2023-05-11 15:46:19 +0300

    много нового

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE users_db;
+USE users_db;
+CREATE TABLE users (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY email (email)
+);
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'your_mysql_username',
+  password: 'your_mysql_password',
+  database: 'users_db',
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as id ' + connection.threadId);
+});
+
+app.post('/register', function(req, res) {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const query = `INSERT INTO users (name, email, password) VALUES ('${name}', '${email}', '${password}')`;
+
+  connection.query(query, function(error, results, fields) {
+    if (error) throw error;
+    res.send('User registered successfully!');
+  });
+});
+
+app.listen(3000, function() {
+  console.log('Server is listening on port 3000!');
+});
